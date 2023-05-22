4fe89309d27ffe9ba65aa4f6d719879ac236f002
pankaj142 <pankajagham1@gmail..com>
2023-05-22 23:32:29 +0530

    init
    initial commit, users service is created and dockerized

---
+++ users/src/index.ts
@@ -0,0 +1,20 @@
+
+import express from "express";
+import { json } from "express";
+
+const app = express();
+app.use(json());
+
+const PORT = 3000;
+
+app.get("/health", (req, res)=>{
+    res.sendStatus(200)
+})
+
+app.get("/api/users/currentuser", (req, res)=>{
+    res.send("Hello there...")
+})
+
+app.listen(3000, ()=>{
+    console.log(`hh Auth service is running on port ${PORT}`)
+})
